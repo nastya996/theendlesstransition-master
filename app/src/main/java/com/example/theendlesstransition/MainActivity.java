20ccb5b9a2a58915e2605764c748d88eb1482b74
nastya996 <shtrezer957@gmail.com>
2019-08-31 21:31:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/theendlesstransition/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.theendlesstransition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView textView = findViewById(R.id.textView);
+        if (textView.getText().equals("")) {
+            int randomNum = new Random().nextInt(100);
+            textView.setText("http://myfile.org/" + randomNum);
+        }
+
+        Button backBtn = findViewById(R.id.back);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Button nextBtn = findViewById(R.id.next);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
